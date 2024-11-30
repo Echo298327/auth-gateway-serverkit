4778cc5bf18028fe28beddb20c5b057486bed0a8
Echo <you@example.com>
2024-11-30 22:49:44 +0200

    update

--- src/auth_gateway_serverkit/keycloak/config.py
+++ src/auth_gateway_serverkit/keycloak/config.py
@@ -9,8 +9,8 @@ class Settings(BaseSettings):
     SERVER_URL: str = os.getenv("SERVER_URL")
     REALM: str = os.getenv("REALM")
     CLIENT_ID: str = os.getenv("CLIENT_ID")
-    KEYCLOAK_USER = os.getenv("KEYCLOAK_USER")
-    KEYCLOAK_PASSWORD = os.getenv("KEYCLOAK_PASSWORD")
+    KEYCLOAK_USER: str = os.getenv("KEYCLOAK_USER")
+    KEYCLOAK_PASSWORD: str = os.getenv("KEYCLOAK_PASSWORD")
 
 
 settings = Settings()
