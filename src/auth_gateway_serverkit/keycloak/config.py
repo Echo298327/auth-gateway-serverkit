c8d5dee2354ed571d3eb03244d019c52650f71c9
Echo <you@example.com>
2024-11-30 23:02:08 +0200

    update

--- src/auth_gateway_serverkit/keycloak/config.py
+++ src/auth_gateway_serverkit/keycloak/config.py
@@ -7,8 +7,9 @@ load_dotenv()
 
 class Settings(BaseSettings):
     SERVER_URL: str = os.getenv("SERVER_URL")
-    REALM: str = os.getenv("REALM")
     CLIENT_ID: str = os.getenv("CLIENT_ID")
+    REALM: str = os.getenv("REALM")
+    SCOPE: str = os.getenv("SCOPE")
     KEYCLOAK_USER: str = os.getenv("KEYCLOAK_USER")
     KEYCLOAK_PASSWORD: str = os.getenv("KEYCLOAK_PASSWORD")
 
